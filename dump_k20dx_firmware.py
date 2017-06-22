79a040c10a4c28861bb0aacba0142c13a222e337
brandonboesch <Brandon.Boesch@arm.com>
2017-06-22 16:08:28 -0500

    added dumps

--- dump_k20dx_firmware.py
+++ dump_k20dx_firmware.py
@@ -1,7 +1,7 @@
 from pyOCD.board import MbedBoard
 
 #specific board used for OCD by setting board_id = '<board id of OCD reported by mbedls command>'. If you set to None, then it will search for one and ask for confirmation.
-board_id = None
+board_id = '10500000e062eef000000000000000000000000097969902'
 
 with MbedBoard.chooseBoard(target_override='k20d50m', board_id=board_id) as board:
   addr = 0x00000000
