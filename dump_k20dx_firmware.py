f65b2e553463ac889f53c3408817ebbf09b74f02
brandonboesch <Brandon.Boesch@arm.com>
2017-07-10 09:59:23 -0500

    updated code to be more user friendly

--- dump_k20dx_firmware.py
+++ dump_k20dx_firmware.py
@@ -2,12 +2,15 @@ from pyOCD.board import MbedBoard
 
 #specific board used for OCD by setting board_id = '<board id of OCD reported by mbedls command>'. If you set to None, then it will search for one and ask for confirmation.
 board_id = '10500000e062eef000000000000000000000000097969902'
+#board_id = None
+
+RIPPED_FILE = "rom.bin"   # name given to rom ripped from device
 
 with MbedBoard.chooseBoard(target_override='k20d50m', board_id=board_id) as board:
   addr = 0x00000000
   size = 128 * 1024   # K20 has 128 KB of flash. 1024 converts to KB
   data = board.target.readBlockMemoryUnaligned8(addr, size)
   data = bytearray(data)
-  with open("rom.bin", 'wb') as f:
+  with open(RIPPED_FILE, 'wb') as f:
       f.write(data)
-  print("Dump success. File rom.bin created")
+  print("Dump success. File " + RIPPED_FILE + " created")
--- restore_k20dx_firmware.py
+++ restore_k20dx_firmware.py
@@ -1,9 +1,13 @@
 from pyOCD.board import MbedBoard
 
+#specific board used for OCD by setting board_id = '<board id of OCD reported by mbedls command>'. If you set to None, then it will search for one and ask for confirmation.
+board_id = '10500000e062eef000000000000000000000000097969902'
+#board_id = None
+
 RECOVERY_FILE = "rom.bin"   # binary to write onto device
 
 # to flash bootloader/interface firmware, set target_override='k20d50m'.  To flash application firmware to target, set target_override='<target board type>'.
-with MbedBoard.chooseBoard(target_override='k20d50m', frequency=10000000) as board:
-  print("Starting flashing")
+with MbedBoard.chooseBoard(target_override='k20d50m', board_id=board_id, frequency=10000000) as board:
+  print("Flashing " + RECOVERY_FILE + " to target")
   board.flash.flashBinary(RECOVERY_FILE)
   print("Recovery successful")
